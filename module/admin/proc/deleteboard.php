afd1aebf00e6ad17a1c22b3e1e0ca463380df03b
phiDel <apps.afox@gmail.com>
2016-06-07 20:48:45 +0900

    게시판 삭제시 md_id를 못 찾던거 수정

--- module/admin/proc/deleteboard.php
+++ module/admin/proc/deleteboard.php
@@ -20,10 +20,10 @@ function proc($data) {
 
 		$md_id = $module['md_id'];
 
-		$sql = 'SELECT wr_srl FROM '._AF_DOCUMENT_TABLE_.' WHERE md_id=:1 OR (md_id=\'_AFOXtRASH_\' AND wr_updater=:2)';
+		$sql = 'SELECT md_id,wr_srl,wr_updater FROM '._AF_DOCUMENT_TABLE_.' WHERE md_id=:1 OR (md_id=\'_AFOXtRASH_\' AND wr_updater=:2)';
 		DB::getList($sql, [$md_id,$md_id], function($r){
 			while ($row = DB::assoc($r)) {
-				$_md_id = $row['md_id'];
+				$_md_id = ($row['md_id'] === '_AFOXtRASH_') ? $row['wr_updater'] : $row['md_id'];
 				$_wr_srl = $row['wr_srl'];
 				// 파일 삭제
 				$variable = ['binary','image','video','audio'];
