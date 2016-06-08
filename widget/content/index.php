742ac4167516e1d3123dc1a9dbed305292ff6181
phiDel <apps.afox@gmail.com>
2016-06-08 20:23:33 +0900

    content widget 에 gallery 타입 추가

--- widget/content/index.php
+++ widget/content/index.php
@@ -8,23 +8,41 @@ if(file_exists($tpl_file)) {
 include $tpl_file;
 } else {
 	if(empty($_WIDGET['module'])) return;
+
+	$type = isset($_WIDGET['type']) ? $_WIDGET['type'] : 'default';
 	$count = isset($_WIDGET['count']) ? (int)$_WIDGET['count'] : 5;
-	$width = isset($_WIDGET['width']) ? $_WIDGET['width'] : '100%';
-	$_list = getDBList(_AF_DOCUMENT_TABLE_,['md_id'=>$_WIDGET['module']],'wr_regdate desc',1,$count);
-	$md_title = getModule($_WIDGET['module'])['md_title'];
+	$style = isset($_WIDGET['style']) ? $_WIDGET['style'] : 'width:100%';
+
+	$md_title = getModule($_WIDGET['module'], 'md_title');
+	if($type === 'gallery') {
+		$_list = DB::getList('SELECT * FROM '._AF_FILE_TABLE_.' WHERE md_id=:1 GROUP BY mf_target ORDER BY mf_regdate desc LIMIT 1,'.$count, [$_WIDGET['module']]);
+	} else {
+		$_list = getDBList(_AF_DOCUMENT_TABLE_,['md_id'=>$_WIDGET['module']],'wr_regdate desc',1,$count);
+	}
 ?>
-<div class="content_widget panel panel-default" style="width:<?php echo $width?>">
+<div class="content_widget panel panel-default" style="<?php echo $style?>">
 	<div class="panel-heading clearfix">
 	<?php echo $md_title ?>
 	<a class="pull-right" href="<?php echo getUrl('','id',$_WIDGET['module'])?>"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></a>
 	</div>
-	<div  class="list-group">
-	<?php
-		foreach ($_list['data'] as $val) {
-			echo '<a class="list-group-item" href="'.getUrl('','id',$val['md_id'],'srl',$val['wr_srl']).'">'.$val['wr_title'].'</a>';
-		}
-	?>
-	</div >
+	<?php if($type === 'gallery') { ?>
+		<div class="panel-body" style="height:150px">
+		<?php
+			$w = (100 / $count);
+			foreach ($_list as $val) {
+				echo '<a href="'.getUrl('','id',$val['md_id'],'srl',$val['mf_target']).'" style="margin:0;padding:0"><img src="./?file='.$val['mf_srl'].'&thumb=100x100" width="'.$w.'%" height="100%" style="margin:0;padding:3px"></a>';
+			}
+		?>
+		</div>
+	<?php } else { ?>
+		<div  class="list-group">
+		<?php
+			foreach ($_list['data'] as $val) {
+				echo '<a class="list-group-item" href="'.getUrl('','id',$val['md_id'],'srl',$val['wr_srl']).'">'.$val['wr_title'].'</a>';
+			}
+		?>
+		</div >
+	<?php } ?>
 </div>
 
 <?php }
--- widget/content/setup.php
+++ widget/content/setup.php
@@ -4,5 +4,24 @@ if(!defined('__AFOX__')) exit();
 
 <h1>위젯 코드 예제</h1>
 <pre>
-&lt;img class="afox_widget" widget="content" style="default" module="board_1" count="5" width="100%"&gt;
+&lt;img class="afox_widget" widget="content" module="아이디"&gt;
+
+* 사용 가능한 옵션 *
+  type      : 목록 타입 (사용가능한 타입 default 또는 gallery)
+  count     : 목록 수
+  style     : 스타일
+</pre>
+
+<h3>예제 1</h3>
+<pre>
+&lt;img class="afox_widget" widget="content" type="default" module="id" count="5"&gt;
+</pre>
+
+<h3>예제 2</h3>
+<pre>
+&lt;div style="text-align:justify"&gt;
+&lt;img class="afox_widget" widget="content" type="default" module="id_1" style="width:48%;display:inline-block"&gt;
+&lt;img class="afox_widget" widget="content" type="default" module="id_2" style="width:48%;display:inline-block"&gt;
+&lt;div style="width:48%;display:inline-block;height:1px"&gt;&lt;/div&gt;
+&lt;/div&gt;
 </pre>
