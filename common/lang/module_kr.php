06040a521165900aa6da599388422a8367c615bf
aFox <apps.afox@gmail.com>
2017-04-08 12:34:22 +0900

    언어파일 정리

--- common/lang/module_kr.php
+++ common/lang/module_kr.php
@@ -2,32 +2,35 @@
 if(!defined('__AFOX__')) exit();
 // 언어엔 ' (홑따옴표) 는 쓰지마세요. 에러가 발생할 가능성이 있습니다. 필요하면 ` (악센트) 이걸 사용하세요.
 
-$_LANG['read_all'] = '모두 읽기';
+$_LANG['agree_member_signup'] = '가입을 동의합니다';
+$_LANG['member_icon'] = '회원 아이콘';
+$_LANG['not_use_signup'] = '지금은 회원 가입을 하실 수 없습니다.';
 $_LANG['permanently_delete'] = '영구히 삭제';
+$_LANG['read_all'] = '모두 읽기';
 $_LANG['removed_date'] = '삭제날짜';
 $_LANG['terms_of_use'] = '이용약관';
-$_LANG['desc_terms_of_use'] = '가입전에 이용약관을 꼭 읽고 가입에 동의해주세요.';
-$_LANG['member_icon'] = '회원 아이콘';
-$_LANG['desc_member_memo'] = '회원 프로필에 표시할 메모를 입력할 수 있습니다. (Markdown 지원)';
-$_LANG['desc_member_icon'] = '회원 아이콘은 가로,세로 100x100 크기의 png 파일로 등록해주세요.';
-$_LANG['not_use_signup'] = '지금은 회원 가입을 하실 수 없습니다.';
 $_LANG['verify_password'] = '비밀번호 확인';
-$_LANG['agree_member_signup'] = '가입을 동의합니다';
-$_LANG['desc_id'] = '아이디는 유일해야 하며 영문, 숫자, 언더바(_)만 사용 가능합니다. (첫 글자는 영문이여야 합니다)';
-$_LANG['desc_new_mb_password'] = '비밀번호와 비밀번호 확인을 입력하세요.';
+
 $_LANG['desc_change_mb_password'] = '비밀번호를 변경하시려면 비밀번호와 확인을 입력하세요.';
-$_LANG['msg_has_been_delete'] = '이 문서는 삭제되었습니다.';
-$_LANG['msg_prohibit_id'] = '이 아이디나 별명은 사용하실 수 없습니다.';
-$_LANG['msg_max_overflow'] = '최대 크기 (%s)를 벗어났습니다.';
-$_LANG['msg_not_welcome_page'] = 'welcome 페이지는 삭제할 수 없습니다.';
-$_LANG['msg_empty_id'] = '회원아이디나 비밀번호가 공백이면 안됩니다.'; //501
+$_LANG['desc_id'] = '아이디는 유일해야 하며 영문, 숫자, 언더바(_)만 사용 가능합니다. (첫 글자는 영문이여야 합니다)';
+$_LANG['desc_member_icon'] = '회원 아이콘은 가로,세로 100x100 크기의 png 파일로 등록해주세요.';
+$_LANG['desc_member_memo'] = '회원 프로필에 표시할 메모를 입력할 수 있습니다. (Markdown 지원)';
+$_LANG['desc_new_mb_password'] = '비밀번호와 비밀번호 확인을 입력하세요.';
+$_LANG['desc_terms_of_use'] = '가입전에 이용약관을 꼭 읽고 가입에 동의해주세요.';
+
 $_LANG['msg_diff_password'] = '새 비밀번호와 비밀번호 확인이 서로 다릅니다.'; //503
-$_LANG['msg_wrong_password2'] = '가입된 회원아이디가 아니거나 비밀번호가 틀립니다.'."\n".'비밀번호는 대소문자를 구분합니다.'; //906
-$_LANG['msg_not_admin'] = '관리자가 아닙니다.'."\n".'다시 로그인해 주십시요.'; //551
-$_LANG['msg_not_write_reply'] = '더 이상 답변하실 수 없습니다.'."\n".'답변은 5단계 각 단계당 93개 까지만 가능합니다.'; //571
-$_LANG['msg_not_delete_reply_with_sub'] = '이 코멘트와 관련된 답변코멘트가 존재하므로 삭제 할 수 없습니다.'; //573
 $_LANG['msg_email_exists'] = '같은 메일이 이미 등록 되어있습니다.'; // 802
+$_LANG['msg_empty_id'] = '회원아이디나 비밀번호가 공백이면 안됩니다.'; //501
+$_LANG['msg_has_been_delete'] = '이 문서는 삭제되었습니다.';
+$_LANG['msg_max_overflow'] = '최대 크기 (%s)를 벗어났습니다.';
 $_LANG['msg_nick_exists'] = '같은 별명이 이미 등록 되어있습니다.'; // 802
+$_LANG['msg_not_admin'] = '관리자가 아닙니다.'."\n".'다시 로그인해 주십시요.'; //551
+$_LANG['msg_not_delete_reply_with_sub'] = '이 코멘트와 관련된 답변코멘트가 존재하므로 삭제 할 수 없습니다.'; //573
+$_LANG['msg_not_welcome_page'] = 'welcome 페이지는 삭제할 수 없습니다.';
+$_LANG['msg_not_write_reply'] = '더 이상 답변하실 수 없습니다.'."\n".'답변은 5단계 각 단계당 93개 까지만 가능합니다.'; //571
+$_LANG['msg_prohibit_id'] = '이 아이디나 별명은 사용하실 수 없습니다.';
+$_LANG['msg_wrong_password2'] = '가입된 회원아이디가 아니거나 비밀번호가 틀립니다.'."\n".'비밀번호는 대소문자를 구분합니다.'; //906
+
 $_LANG['success_member_signup'] = '회원에 가입되셨습니다.'."\n".'다시 로그인해주세요.';
 
 /* End of file module_kr.php */
