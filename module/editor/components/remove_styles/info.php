9f4687eccea7ab1a88da6c682edffdecc12a3385
phiDel <apps.afox@gmail.com>
2017-10-24 23:42:13 +0900

    에디터 콤포넌트 "스타일 제거" 추가

---
+++ module/editor/components/remove_styles/info.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('__AFOX__')) exit();
+
+$_COMPONENT_INFO['version'] = "1.0";
+$_COMPONENT_INFO['author'] = "aFox";
+$_COMPONENT_INFO['email'] = "support@afox.kr";
+$_COMPONENT_INFO['link'] = "http://afox.kr";
+$_COMPONENT_INFO['title'] = "스타일 삭제";
+$_COMPONENT_INFO['description'] = "HTML 태그에 붙은 불필요한 스타일및 속성 항목을 삭제합니다.";
+$_COMPONENT_INFO['date'] = "2017-10-23";
+
+/* End of file info.php */
+/* Location: ./module/editor/components/remove_styles/info.php */
