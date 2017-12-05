66063bd165e443bcab55bb59d9a8e27f85747f2b
phiDel <apps.afox@gmail.com>
2017-12-05 22:45:14 +0900

    file 라이브러리 업데이트

--- lib/file/file.php
+++ lib/file/file.php
@@ -1,118 +1,105 @@
 <?php
 if(!defined('__AFOX__')) exit();
 require_once dirname(__FILE__) . '/../../initial/config.php';
-function setHttpError($err, $back = false) {
-	header('HTTP/1.1 '.$err);
+function setHttpError($e,$b=false){
+	header('HTTP/1.1 '.$e);
 	header("Connection: close");
-	if($back) {
-		$_SESSION['AF_VALIDATOR_ERROR'] = ['error'=>3,'message'=>'HTTP/1.1 '.$err];
-		header('Location: ' . $_SERVER['HTTP_REFERER']); // binary 는 뒤로 가기
+	if($b){
+		$_SESSION['AF_VALIDATOR_ERROR']=['error'=>3,'message'=>'HTTP/1.1 '.$e];
+		header('Location: '.$_SERVER['HTTP_REFERER']);
 	}
 	exit;
 }
-function getLogin($f='mb_srl') {
-	$mbid = isset($_SESSION['AF_LOGIN_ID']) ? $_SESSION['AF_LOGIN_ID'] : get_cookie('AF_LOGIN_ID');
-	if(!empty($mbid) && preg_match('/^[a-zA-Z]+\w{2,}$/', $mbid)) {
-		$mb = DB::get("SELECT * FROM "._AF_MEMBER_TABLE_." WHERE mb_id = '{$mbid}'");
-		if(!DB::error() && !empty($mb['mb_srl'])) return $mb[$f];
+$mb_srl = 0;
+$mb_rank = '0';
+if(!empty($tmp=isset($_SESSION['AF_LOGIN_ID'])?$_SESSION['AF_LOGIN_ID']:get_cookie('AF_LOGIN_ID'))&&preg_match('/^[a-zA-Z]+\w{2,}$/',$tmp)){
+	$out=DB::get("SELECT * FROM "._AF_MEMBER_TABLE_." WHERE mb_id = '{$tmp}'");
+	if(!DB::error()&&!empty($out['mb_srl'])){
+		$mb_srl = $out['mb_srl'];
+		$mb_rank = $out['mb_rank'];
 	}
-	return '0';
 }
-if($_CFG['use_full_login']==1&&empty((int)getLogin())) setHttpError('401 Unauthorized');
+if($_CFG['use_full_login']==1&&empty($mb_srl)) setHttpError('401 Unauthorized');
 if($_CFG['use_protect']==1&&!empty($_SERVER['HTTP_REFERER'])&&!preg_match('/^https?:[\/]+[a-z0-9\-\.]*'.$_SERVER['SERVER_NAME'].'.+/i',$_SERVER['HTTP_REFERER'])) setHttpError('401 Unauthorized');
-static $_file = [];
+static $_f = [];
 $srl = (int)$_GET['file'];
 $thumb = isset($_GET['thumb']);
 $key = $srl.($thumb?'_thumb'.$_GET['thumb']:'');
-if(!isset($_file[$key])) {
-	$file = DB::assoc(DB::select(_AF_FILE_TABLE_,['mf_srl'=>$srl]));
-	if(!empty($file['error'])) setHttpError('400 Bad Request');
-	$filetype=strtolower(array_shift(explode('/',$file['mf_type'])));
-	$_tmp=array('binary'=>0,'image'=>1,'video'=>2,'audio'=>3);
-	$_file[$key]=['mb_srl'=>$file['mb_srl'], 'is_download'=>true, 'point_download'=>0, 'type'=>empty($_tmp[$filetype])?'binary':$filetype, 'name'=>$file['mf_name']];
-	$_file[$key]['path']=_AF_ATTACH_DATA_.$_file[$key]['type'].'/'.$file['md_id'].'/'.$file['mf_target'].'/'.$file['mf_upload_name'];
-	// binary면 권한 체크 // isGrant() 함수 안불러서 작성
-	if($_file[$key]['type']=='binary') {
-		$out = DB::select(_AF_MODULE_TABLE_, ['md_id'=>$file['md_id']], ['md_id','point_download','grant_download']);
-		$module = DB::assoc($out);
+if(!isset($_f[$key])) {
+	$out = DB::assoc(DB::select(_AF_FILE_TABLE_,['mf_srl'=>$srl]));
+	if(!empty($out['error'])) setHttpError('400 Bad Request');
+	$ft=strtolower(array_shift(explode('/',$out['mf_type'])));
+	$fts=array('binary'=>0,'image'=>1,'video'=>2,'audio'=>3);
+	$_f[$key]=['mb_srl'=>$out['mb_srl'], 'permission'=>true, 'point'=>0, 'mime'=>$out['mf_type'], 'type'=>empty($fts[$ft])?'binary':$ft, 'name'=>$out['mf_name']];
+	$_f[$key]['path']=_AF_ATTACH_DATA_.$_f[$key]['type'].'/'.$out['md_id'].'/'.$out['mf_target'].'/'.$out['mf_upload_name'];
+	if($_f[$key]['type']=='binary') { // binary면 권한 체크 // isGrant() 함수 안불러서 작성
+		$module = DB::assoc(DB::select(_AF_MODULE_TABLE_, ['md_id'=>$out['md_id']], ['md_id','point_download','grant_download']));
 		if(empty($module['md_id'])) setHttpError('400 Bad Request', true);
-		$_file[$key]['point_download'] = (int)$module['point_download'];
-		$grant = $module['grant_download'];
-		if(!empty($grant)) {
-			$rank = ord(getLogin('mb_rank'));
-			$_file[$key]['is_download'] = ord($grant) <= $rank; // 0 = 48, z = 122 // s = 115 초과면 블럭 대상임
-			if($rank > 115 || !$_file[$key]['is_download']) setHttpError('401 Unauthorized', true);
+		$_f[$key]['point'] = (int)$module['point_download'];
+		$_f[$key]['permission'] = !($_f[$key]['point'] < 0 && empty($mb_srl)); // 포인트가 -면 비회원 다운 불가
+		if($_f[$key]['permission']&&!empty($grant = $module['grant_download'])) {
+			$tmp = ord($mb_rank);
+			$_f[$key]['permission'] = $tmp < 116 && ord($grant) <= $tmp; // 0 = 48, z = 122 // s = 115 초과면 에러
+			if(!$_f[$key]['permission']) setHttpError('401 Unauthorized', true);
 		}
-	} else if($thumb&&$_file[$key]['type']=='image') {
-		if(!file_exists($_file[$key]['path'])) {
-			$_file[$key]['path']=_AF_PATH_.'common/img/no_image.png';
+	} else if($thumb&&$_f[$key]['type']=='image') {
+		if(!file_exists($_f[$key]['path'])) {
+			$_f[$key]['path']=_AF_PATH_.'common/img/no_image.png';
 		} else {
-			$size = $_GET['thumb'];
-			if(empty($size)) {
-				// 썸네일 사이즈가 빈값이면 모듈설정 사용
-				$out = DB::select(_AF_MODULE_TABLE_, ['md_id'=>$file['md_id']], ['md_id','thumb_width','thumb_height','thumb_option']);
-				$module = DB::assoc($out);
+			if(empty($size = $_GET['thumb'])) { // 썸네일 사이즈가 빈값이면 모듈설정 사용
+				$module = DB::assoc(DB::select(_AF_MODULE_TABLE_, ['md_id'=>$out['md_id']], ['md_id','thumb_width','thumb_height','thumb_option']));
 				if(empty($module['md_id'])) setHttpError('400 Bad Request', true);
 				$tw = (int)$module['thumb_width'];
 				$th = (int)$module['thumb_height'];
 				$fit = $module['thumb_option']==='1';
-			} else {
-				// 사용자 썸네일 사이즈는 2가지 크기만
+			} else { // 사용자 썸네일 사이즈는 2가지 크기만
 				$size = explode('x', $size);
-				$tw = (int)$size[0];
-				$th = (int)$size[1];
-				$fit = (int)$size[2] === 1;
-				$tw = $tw>100?200:($tw>0?100:0);
-				$th = $th>100?200:($th>0?100:0);
+				$tw = $size[0]>100?200:($size[0]>0?100:0);
+				$th = $size[1]>100?200:($size[1]>0?100:0);
+				$fit = $size[2]==='1';
 			}
 			if(!empty($tw)&&!empty($th)) {
-				$thumb_file=_AF_ATTACH_DATA_.'thumbnail/'.$file['md_id'].'/'.$file['mf_target'].'/'.$file['mf_srl'].'_'.$tw.'x'.$th.'x'.($fit?'1':'0').'.png';
-				if(file_exists($thumb_file)) {
-					$_file[$key]['path']=$thumb_file;
+				if(file_exists($tmp=_AF_ATTACH_DATA_.'thumbnail/'.$out['md_id'].'/'.$out['mf_target'].'/'.$out['mf_srl'].'_'.$tw.'x'.$th.'x'.($fit?'1':'0').'.png')) {
+					$_f[$key]['path']=$tmp;
 				} else {
 					require_once dirname(__FILE__) . '/thumbnail.php';
-					$_file[$key]['path']=thumbnail($_file[$key]['path'],$thumb_file,$tw,$th,$fit);
+					$_f[$key]['path']=thumbnail($_f[$key]['path'],$tmp,$tw,$th,$fit);
 				}
 			}
 		}
 	}
 }
-if(!$_file[$key]['is_download']) setHttpError('401 Unauthorized', true); // binary 다운로드 불가면 에러
-if(!$fp = @fopen($_file[$key]['path'], 'rb')) setHttpError('404 Not Found');
+if(!$_f[$key]['permission']) setHttpError('401 Unauthorized', true); // binary 다운로드 불가면 에러
+//if($_f[$key]['type']=='binary' && !triggerCall('before_proc', 'fileDownload', $_f[$key])) setHttpError('401 Unauthorized', true); // binary 트리거 호출
+if(!$fp = @fopen($_f[$key]['path'], 'rb')) setHttpError('404 Not Found');
 $fstat=fstat($fp);
 if(!empty($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
-	$modifiedSince=strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
-	if($modifiedSince&&($modifiedSince>=$fstat['mtime'])){
+	$tmp=strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
+	if($tmp&&($tmp>=$fstat['mtime'])){
 		fclose($fp);
 		setHttpError('304 Not Modified');
 	}
 }
-// 다운로드 조회를 위해 기록 // setHistoryAction() 함수 안불러서 작성
-if($_file[$key]['type']=='binary'){
-	$point = $_file[$key]['point_download'];
-	$mb_srl = (int)getLogin();
-	if(empty($mb_srl) && $point < 0) setHttpError('401 Unauthorized', true); // -값은 비회원 불가
+if($_f[$key]['type']=='binary'){ // 다운로드 조회를 위해 기록 setHistoryAction() 함수 안불러서 작성
 	$act = 'mf_download';
 	$uinfo = ['mb_srl'=>$mb_srl,'ipaddress'=>$_SERVER['REMOTE_ADDR']];
 	$pkey = ($uinfo['mb_srl'] > 0 ? 'mb_srl':'mb_ipaddress');
 	$pval = ($uinfo['mb_srl'] > 0 ? $uinfo['mb_srl']:$uinfo['ipaddress']);
-	$_tmp = DB::select(_AF_HISTORY_TABLE_,['hs_action'=>$act.'('.$srl.')',$pkey=>$pval]);
+	$out = DB::select(_AF_HISTORY_TABLE_,['hs_action'=>$act.'('.$srl.')',$pkey=>$pval]);
 	if(!DB::error()) {
-		if(is_null($uinfo['data'] = DB::assoc($_tmp))) {
-			// 처음 한번만 포인트 사용 // 자신은 포인트 사용 안함
-			if(!empty($point) && !empty($mb_srl) && ($mb_srl !== $_file[$key]['mb_srl'])) {
+		if(is_null($uinfo['data'] = DB::assoc($out))) { // 처음 한번만 포인트 사용 // 자신은 포인트 사용 안함
+			if(!empty($point = $_f[$key]['point']) && !empty($mb_srl) && ($mb_srl !== $_f[$key]['mb_srl'])) {
 				$mb = DB::get('SELECT mb_point FROM '._AF_MEMBER_TABLE_.' WHERE mb_srl='.$mb_srl);
 				if(DB::error() || ($mb['mb_point']+$point) < 0) setHttpError('401 Unauthorized', true); // 포인트 모자르면 에러
 				DB::update(_AF_MEMBER_TABLE_,['(mb_point)'=>'mb_point'.($point>0?'+':'').$point],['mb_srl'=>$mb_srl]);
 			}
-			if(true === DB::insert(_AF_HISTORY_TABLE_,['mb_srl'=>$uinfo['mb_srl'],'mb_ipaddress'=>$uinfo['ipaddress'],'hs_action'=>$act.'('.$srl.')','(hs_regdate)'=>'NOW()'])){
-				DB::update(_AF_FILE_TABLE_, ['(mf_download)'=>'mf_download+1'], ['mf_srl'=>$srl]);
-			}
+			DB::insert(_AF_HISTORY_TABLE_,['mb_srl'=>$uinfo['mb_srl'],'mb_ipaddress'=>$uinfo['ipaddress'],'hs_action'=>$act.'('.$srl.')','(hs_regdate)'=>'NOW()']);
+			DB::update(_AF_FILE_TABLE_, ['(mf_download)'=>'mf_download+1'], ['mf_srl'=>$srl]);
 		}
 	} else setHttpError('500 Internal Server Error', true);
 }
 header('Last-Modified: '.$fstat['mtime']);
-header("Content-Disposition: attachment; filename=".$_file[$key]['name']);
+header("Content-Disposition: attachment; filename=".$_f[$key]['name']);
 header('Cache-Control:');
 header('Content-Type: text/plain');
 header("Connection: close");
