00525108cabb5e9f9a3f13835a5a43e2ad161151
phiDel <apps.afox@gmail.com>
2018-01-24 22:29:20 +0900

    에드온 컨텐츠 필터 업데이트

---
+++ addon/content_filter/info.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('__AFOX__')) exit();
+
+$_ADDON_INFO['version'] = "1.0";
+$_ADDON_INFO['author'] = "aFox";
+$_ADDON_INFO['email'] = "support@afox.kr";
+$_ADDON_INFO['link'] = "http://afox.kr";
+$_ADDON_INFO['title'] = "금지 단어 필터";
+$_ADDON_INFO['description'] = "금지 단어가 포함되있으면 특정 단어로 바꿔주는 에드온입니다.";
+$_ADDON_INFO['date'] = "2016-05-12";
+
+/* End of file info.php */
+/* Location: ./addon/content_filter/info.php */
