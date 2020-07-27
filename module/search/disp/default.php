12c1f1de0d21777101358f47f6034d149a59060c
파이델 <apps.afox@gmail.com>
2020-07-27 11:48:58 +0900

    Update default.php

--- module/search/disp/default.php
+++ module/search/disp/default.php
@@ -22,7 +22,7 @@ function proc($data) {
 
 	$schs = [];
 	if(!empty($search)) {
-		$schkeys = ['title'=>'wr_title','content'=>'wr_content','nick'=>'mb_nick','tag'=>'wr_tags','date'=>'wr_regdate'];
+		$schkeys = [''=>'wr_title','title'=>'wr_title','content'=>'wr_content','nick'=>'mb_nick','tag'=>'wr_tags','date'=>'wr_regdate'];
 		$ss = explode(':', $search);
 		if(count($ss)>1 && !empty($schkeys[$ss[0]])) {
 			$search = trim(implode(':', array_slice($ss,1)));
