84bd016ed3c0f09976935cfc9e390c0e5c3d35a7
phiDelPark <apps.afox@gmail.com>
2024-02-01 04:00:38 +0900

    날짜 검색 수정

--- module/searchex/disp/default.php
+++ module/searchex/disp/default.php
@@ -10,8 +10,7 @@ function proc($data)
 	if (empty($mids)) $mids[] = "@@@@@@@@@@"; // 검색할 모듈이 없으면 임시 값
 	$_wheres = ["md_id{IN}" => implode(",", $mids) , "(_AND_)" => [], "(_OR_)" => []];
 
-	if (!empty($search))
-	{
+	if (!empty($search)) {
 		$keys = [":" => "wr_title", //:title
 			"@" => "mb_nick", //@nick
 			"#" => "wr_tags", //#tag
@@ -22,21 +21,21 @@ function proc($data)
 		empty($key) ? ($key = "wr_content") : ($search = substr($search, 1));
 		$search = explode(" ", trim($search));
 
-		if (!empty($search))
-		{
+		if (!empty($search)) {
+
 			$index = 0;
-			foreach ($search as $value)
-			{
+			foreach ($search as $value) {
 				$value = explode("&", trim($value));
 				$and_or = count($value) > 1 ? "(_AND_)" : "(_OR_)";
-				foreach ($value as $v)
-				{
-					if ($key == "wr_regdate")
-					{
+
+				foreach ($value as $v) {
+					if ($key == "wr_regdate") {
 						$v = str_split($v, 4);
 						$v = $v[0] . (empty($v[1]) ? "" : "-" . implode("-", str_split($v[1], 2)));
+					} else {
+						$v = "%" . $v;
 					}
-					$_wheres[$and_or][$key . "{LIKE}[" . $index++ . "]"] = "%" . $v . "%";
+					$_wheres[$and_or][$key . "{LIKE}[" . $index++ . "]"] = $v . "%";
 				}
 			}
 		}
