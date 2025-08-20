b5835348da52892fce5a1d5e265b3343c5b5eed5
PHIDEL <apps.afox@gmail.com>
2025-08-21 06:46:05 +0900

    mf_about null 허용

--- install/index.php
+++ install/index.php
@@ -363,7 +363,7 @@ $create_sql = '
 	   md_id           CHAR(11)     NOT NULL,
 	   mf_target       INT(11)      NOT NULL,
 	   mf_name         VARCHAR(255) NOT NULL,
-	   mf_about        VARCHAR(255) NOT NULL,
+	   mf_about        VARCHAR(255),
 	   mf_upload_name  VARCHAR(32)  NOT NULL,
 	   mf_type         VARCHAR(128) NOT NULL,
 	   mf_link         CHAR(1)      NOT NULL DEFAULT 0,
